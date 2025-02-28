bd02eca061efe44fdc49370d01108a87e4b4bed7
gabrielarnaudo <s343557@studenti.polito.it>
2025-02-28 17:03:48 +0100

    Cambio in cartelle

---
+++ Lab01/part2/mux.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+USE ieee.std_logic_1164.all;
+
+entity mux is 
+port ( SW : in std_logic_vector(8 downto 0);
+LEDR : out std_logic_vector(3 downto 0));
+end mux;
+
+architecture beh of mux is 
+begin
+--ciao
+process (SW(8 downto 0))
+begin 
+if SW(8) = '1' then 
+LEDR(3 downto 0) <= SW(7 downto 4);
+else
+LEDR(3 downto 0) <= SW(3 downto 0);
+end if;
+end process;
+end beh;
