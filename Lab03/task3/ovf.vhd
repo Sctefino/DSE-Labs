0f3f0fca7265c0b130699e409652cb0be17fb90a
Sctefino <stefano.crescenza@gmail.com>
2025-03-18 11:04:33 +0100

    Modifica overflow

--- Lab03/task3/ovf.vhd
+++ Lab03/task3/ovf.vhd
@@ -2,14 +2,12 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity ovf is
-	port( c_o, a_msb, b_msb, s_msb: in std_logic;
-	      ov: out std_logic
-	);
+	port(a_msb, b_msb, s_msb: in std_logic;
+	      ov: out std_logic);
 end ovf;
 
 architecture beh of ovf is
 begin
-	ov <= c_o or (not(a_msb) and not(b_msb) and s_msb); --overflow se carry out di ultimo fa oppure se
-							    --ultimo fa ha valori positivi all'ingresso e 
-							    --risultato e' negativo
+	ov <= (not(a_msb) and not(b_msb) and s_msb) or (a_msb and b_msb and not(s_msb)); -- overflow se somma di valori concordi da risultato di segno opposto
+																												-- altrimenti no overflow
 end beh;
