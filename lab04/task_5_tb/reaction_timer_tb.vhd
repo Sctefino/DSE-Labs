b9f36e5920121946f851b959f626596b79bacf94
Sctefino <115326088+Sctefino@users.noreply.github.com>
2025-03-28 16:56:46 +0100

    Testbench con uscita clock 1 kHZ

--- lab04/task_5_tb/reaction_timer_tb.vhd
+++ lab04/task_5_tb/reaction_timer_tb.vhd
@@ -11,17 +11,19 @@ port (clock_50 : in std_logic;
 	sw : in std_logic_vector(7 downto 0);
 	key : in std_logic_vector(3 downto 0);
 	ledr : out std_logic_vector(9 downto 0);
+	clk_kHz: out std_logic;
 	hex0,hex1,hex2,hex3 : out std_logic_vector(6 downto 0));
 end component;
 
 signal clock_50_tb: std_logic;
 signal sw_tb: std_logic_vector(7 downto 0);
-signal key_tb: std_logic_vector(3 downto 0):= (others => '0');
+signal key_tb: std_logic_vector(3 downto 0):= (others => '1');
 signal ledr_tb: std_logic_vector(9 downto 0);
+signal clk_tb: std_logic;
 signal hex0_tb, hex1_tb, hex2_tb, hex3_tb: std_logic_vector(6 downto 0);
 
 begin
-sw_tb <= "00001010"; --fisso ritardo iniziale a 10 ms
+sw_tb <= "00001011"; --fisso ritardo iniziale a 10 ms
 clock: process
 begin
 clock_50_tb <= '0';
@@ -32,15 +34,15 @@ end process;
 
 signals: process
 begin
-key_tb(0) <= '1';
-wait for 10 ns;
 key_tb(0) <= '0';
+wait for 10 ns;
+key_tb(0) <= '1';
 wait for 50 ms;
-key_tb(3) <= '1';
-wait for 5 ns;
 key_tb(3) <= '0';
+wait for 5 ns;
+key_tb(3) <= '1';
 wait;
 end process;
 
-testbench: reaction_timer port map(clock_50_tb, sw_tb, key_tb, ledr_tb, hex0_tb, hex1_tb, hex2_tb, hex3_tb);
+testbench: reaction_timer port map(clock_50_tb, sw_tb, key_tb, ledr_tb, clk_tb, hex0_tb, hex1_tb, hex2_tb, hex3_tb);
 end beh;
