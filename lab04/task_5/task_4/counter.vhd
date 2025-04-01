4d79f582f8f44607fe55be91b23fc78f5e5aa60a
Sctefino <stefano.crescenza@gmail.com>
2025-04-01 17:47:40 +0200

    Cambio counter

--- lab04/task_5/task_4/counter.vhd
+++ lab04/task_5/task_4/counter.vhd
@@ -4,28 +4,28 @@ use ieee.numeric_std.all;
 
 entity counter is
 Generic (N : integer := 26);
-port (ck, rstn, clear: in std_logic;
+port (ck, rstn, clear, enable: in std_logic;
 		b: out std_logic_vector(N-1 downto 0));
 end counter;
 
 architecture beh of counter is
 
 signal sum : unsigned(N-1 downto 0) := (others => '0');
-signal add : unsigned(N-1 downto 0) := (others => '0');
 
 begin
 process (ck, rstn)
 begin
-if rstn = '0' then
-sum <= (others => '0');
-elsif rising_edge(ck) then
-if clear = '1' then
-sum <= (others => '0');
-else
-sum <= sum + add;
-end if;
-end if;
+	if rstn = '0' then
+		sum <= (others => '0');
+	elsif rising_edge(ck) then
+		if clear = '1' then
+			sum <= (others => '0');
+		elsif enable = '1' then
+			sum <= sum + 1;		
+		end if;
+	end if;
+
 end process;
-add(0) <= '1';
+
 b <= std_logic_vector(sum);
 end beh;
--- lab04/task_5/task_4/flash_digit.vhd
+++ lab04/task_5/task_4/flash_digit.vhd
@@ -12,7 +12,7 @@ architecture str of flash_digit is
 
 component counter
 Generic (N : integer := 26);
-port (ck, rstn, clear: in std_logic;
+port (ck, rstn, clear, enable: in std_logic;
 		b: out std_logic_vector(N-1 downto 0));
 end component;
 
@@ -34,11 +34,11 @@ signal clear1,clear2: std_logic;
 begin
 count1: counter
 generic map (N => 26)
-port map (clock_50, key(0), clear1, add1out);
+port map (clock_50, key(0), clear1, '1', add1out);
 
 count2: counter
 generic map (N => 4)
-port map (clear1, key(0), clear2, add2out);
+port map (clock_50, key(0), clear2, clear1, add2out);
 
 comp1: comparator
 generic map (N => 26)
