2345622c77f2b5b44bcb86c350bc470b70bb9712
gabrielarnaudo <s343557@studenti.polito.it>
2025-04-13 15:54:53 +0200

    Update fsm.vhd

--- LAB05/task3/fsm.vhd
+++ LAB05/task3/fsm.vhd
@@ -1,79 +1,101 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+
 ENTITY fsm IS
-PORT (key : in std_logic_vector(3 downto 0);
-sw : in std_logic_vector(1 downto 0);
-ledr : out std_logic_vector(9 downto 0));
+    PORT (
+        key  : in std_logic_vector(3 downto 0);
+        sw   : in std_logic_vector(1 downto 0);
+        ledr : out std_logic_vector(9 downto 0)
+    );
 END fsm;
 
 ARCHITECTURE Behavior OF fsm IS
-TYPE State_type IS (A, B, C, D, E, F, G, H, I);
-SIGNAL y_Q, Y_D : State_type; -- y_Q is present state, y_D is next state
-
-
+    TYPE State_type IS (A, B, C, D, E, F, G, H, I);
+    SIGNAL y_Q, Y_D : State_type := A;  -- declare type
 BEGIN
-
-PROCESS (y_Q) -- state table
-
+PROCESS (y_Q, sw)
 BEGIN
+Y_D <= y_Q; --get rid of latches
 CASE y_Q IS
- when A => if (sw(1) = '0') then Y_D <= B;
- ELSE Y_D <= F;
- END IF; 
- 
- when B => if (sw(1) = '0') then Y_D <= C;
- ELSE Y_D <= F;
- END IF;
- 
- when C => if (sw(1) = '0') then Y_D <= D;
- ELSE Y_D <= F;
- END IF;
- 
- when D => if (sw(1) = '0') then Y_D <= E;
- ELSE Y_D <= F;
- END IF;
- 
- when E => if (sw(1) = '0') then Y_D <= F;
- END IF;
- 
- when F => if (sw(1) = '0') then Y_D <= B;
- ELSE Y_D <= G;
- END IF;
- 
- when G => if (sw(1) = '0') then Y_D <= B;
- ELSE Y_D <= H;
- END IF;
- 
- when H => if (sw(1) = '0') then Y_D <= B;
- ELSE Y_D <= I;
- END IF;
- 
- when I => if (sw(1) = '0') then Y_D <= B;
- END IF;
- 
- when others => Y_D <= A;
+WHEN A =>
+IF sw(1) = '0' THEN
+Y_D <= B;
+ELSE
+Y_D <= F;
+END IF;
+WHEN B =>
+IF sw(1) = '0' THEN
+Y_D <= C;
+ELSE
+Y_D <= F;
+END IF;
+WHEN C =>
+IF sw(1) = '0' THEN
+Y_D <= D;
+ELSE
+Y_D <= F;
+END IF;
+WHEN D =>
+IF sw(1) = '0' THEN
+Y_D <= E;
+ELSE
+Y_D <= F;
+END IF;
+WHEN E =>
+IF sw(1) = '0' THEN
+Y_D <= F;
+ELSE
+Y_D <= E;
+END IF;
+WHEN F =>
+IF sw(1) = '0' THEN
+Y_D <= B;
+ELSE
+Y_D <= G;
+END IF;
+WHEN G =>
+IF sw(1) = '0' THEN
+Y_D <= B;
+ELSE
+Y_D <= H;
+END IF;
+WHEN H =>
+IF sw(1) = '0' THEN
+Y_D <= B;
+ELSE
+Y_D <= I;
+END IF;
+WHEN I =>
+IF sw(1) = '0' THEN
+Y_D <= B;
+ELSE
+Y_D <= I;
+END IF;
+WHEN OTHERS =>
+Y_D <= A;
 END CASE;
-END PROCESS; -- state table
-
-PROCESS (KEY(0)) -- state flip-flops
-
-BEGIN
-
-if Key(0) = '1' and key(0)'event then
-if sw(0) = '1' then
-Y_Q <= A;
-else
-Y_Q <= Y_D;
-end if;
-end if;
 END PROCESS;
-ledr(9 downto 1) <= (others =>'0');
-process(Y_Q)
-begin
-if Y_Q = I or Y_Q = E then
+
+ --synchronize FF
+PROCESS (key(0))
+BEGIN
+IF rising_edge(key(0)) THEN
+IF sw(0) = '1' THEN
+y_Q <= A;  -- reset
+ELSE
+y_Q <= Y_D;
+END IF;
+END IF;
+END PROCESS;
+
+-- Output LEDR
+ledr(9 downto 1) <= (others => '0');
+PROCESS (y_Q)
+BEGIN
+IF y_Q = E OR y_Q = I THEN
 ledr(0) <= '1';
-else
+ELSE
 ledr(0) <= '0';
-end if;
-end process;
-END Behavior; 
+END IF;
+END PROCESS;
+END Behavior;
