d373e223f2639c1ee9b787287d9b4511609bae56
gabrielarnaudo <s343557@studenti.polito.it>
2025-05-05 18:59:54 +0200

    Update main.c

--- Lab_08/task_3/main.c
+++ Lab_08/task_3/main.c
@@ -92,7 +92,7 @@ int main(void)
   LL_TIM_WriteReg(TIM3, PSC, LL_TIM_ReadReg(TIM3, PSC) | 0x10);
   LL_TIM_WriteReg(TIM3, ARR, LL_TIM_ReadReg(TIM3, ARR) | 0xfff);
   LL_TIM_WriteReg(TIM3, CNT, LL_TIM_ReadReg(TIM3, CNT) | 0x00);
-  LL_TIM_WriteReg(TIM3, CR1, LL_TIM_ReadReg(TIM3, CR1) | 0x11);        // max overflow, not really used
+  LL_TIM_WriteReg(TIM3, CR1, LL_TIM_ReadReg(TIM3, CR1) | 0x11);     
   LL_TIM_WriteReg(TIM3, CCR1, LL_TIM_ReadReg(TIM3, CCR1) | 0xc8);         // first target for CH1
   LL_TIM_WriteReg(TIM3, CCR2, LL_TIM_ReadReg(TIM3, CCR2) | 0x28);            // first target for CH2
 
